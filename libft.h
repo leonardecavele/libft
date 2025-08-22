cf19c459ad6c14b31478ba4c70703b46165d45f2
leonardecavele <leonard.decavele@gmail.com>
2025-08-22 13:36:43 +0200

    new prototypes

--- libft.h
+++ libft.h
@@ -1,5 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 11:37:21 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/08/22 13:35:56 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <stddef.h>
 
 typedef struct s_list
 {
@@ -7,7 +21,15 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-//lst
+//libc
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+
+//bonus
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
