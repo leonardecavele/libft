609387c78497f3261b488beb8a89dd5a2577c10a
leonardecavele <leonard.decavele@gmail.com>
2025-08-24 21:33:56 +0200

    added strlcpy and edited 'dest' to 'dst' for memcpy and memmove

---
+++ ft_strlcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 16:02:25 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/07/10 17:05:33 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	while (i + 1 < dstsize && src[i])
+		*dst++ = src[i++];
+	if (dstsize)
+		*dst = 0;
+	return (ft_strlen(src));
+} 
--- libft.h
+++ libft.h
@@ -30,8 +30,9 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 
 //bonus
 t_list	*ft_lstnew(void *content);
