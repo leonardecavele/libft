7a8896d86ebf3f29c72cc9158e911dd2c1006aea
leonardecavele <leonard.decavele@gmail.com>
2025-08-24 21:33:27 +0200

    edited 'dest' to 'dst'

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,20 +12,20 @@
 
 #include "libft.h"
 
-static void	r_cpy(void *dest, const void *src, size_t n)
+static void	r_cpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
 	i = -1;
 	while (++i < n)
-		((unsigned char *)dest)[i] = ((const unsigned char *)src)[i];
+		((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
 }
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	if (dest < src)
-		r_cpy(dest, src, n);
+	if (dst < src)
+		r_cpy(dst, src, n);
 	else
-		ft_memcpy(dest, src, n);
-	return (dest);
+		ft_memcpy(dst, src, n);
+	return (dst);
 }
