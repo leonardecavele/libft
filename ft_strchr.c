003acdd54dbd46dc10538d6779855665df6c9341
leonardecavele <leonard.decavele@gmail.com>
2025-08-30 19:10:40 +0200

    fix: typo on casts to unsigned char

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 18:16:32 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/08/30 18:57:09 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/08/30 19:09:49 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
-		if ((t_u8)*s == (t_u8)c)
+		if ((t_u8)(*s) == (t_u8)c)
 			return ((char *)s);
 		s++;
 	}
-	if ((t_u8)!c)
+	if (!(t_u8)c)
 		return ((char *)s);
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 18:41:44 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/08/30 18:58:43 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/08/30 19:10:09 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strrchr(const char *s, int c)
 	res = NULL;
 	while (*s)
 	{
-		if ((t_u8)*s == (t_u8)c)
+		if ((t_u8)(*s) == (t_u8)c)
 			res = (char *)s;
 		s++;
 	}
-	if ((t_u8)!c)
+	if (!(t_u8)c)
 		return ((char *)s);
 	return (res);
 }
