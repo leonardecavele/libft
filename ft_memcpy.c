9ddbbba9ff7bb402ed5d63848ca1e93ff2010e35
leonardecavele <leonard.decavele@gmail.com>
2025-08-30 20:34:45 +0200

    fix: added a null security

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 19:06:52 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/08/28 11:20:53 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/08/30 20:11:39 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	if (!dst && !src)
+		return (NULL);
 	while (n--)
 		((t_u8p)dst)[n] = ((t_u8p)src)[n];
 	return (dst);
