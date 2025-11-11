ad4ae392122a82067c69fe3043dae4d92e7e2e7f
Leonard Decavele <ldecavel@z2r9p5.42lyon.fr>
2025-11-11 16:16:33 +0100

    fix: better calloc true to the original

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 13:03:53 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/11 15:53:51 by ldecavel         ###   ########lyon.fr   */
+/*   Updated: 2025/11/11 16:13:17 by ldecavel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 	int		len;
 
-	if (!nmemb || !size || (size && nmemb > SIZE_MAX / size))
+	if (size && nmemb > SIZE_MAX / size)
 		return (NULL);
 	len = size * nmemb;
 	ptr = malloc(len);
