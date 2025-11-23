706fef3f70ea9dad7189bc41ee2da387193087af
leonardecavele <leonard.decavele@gmail.com>
2025-11-23 19:28:44 +0100

    fast strlen is on

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,35 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 12:23:18 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/04 13:07:09 by ldecavel         ###   ########lyon.fr   */
+/*   Updated: 2025/11/23 19:27:10 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#define LBYTE 0x0101010101010101
+#define FBYTE 0x8080808080808080
+
 size_t	ft_strlen(const char *s)
 {
-	size_t	len;
+	size_t	map;
+	char	*start;
 
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
+	start = (char *)s;
+	while (*s && (uintptr_t)s % sizeof(size_t))
+		s++;
+	while (*s)
+	{
+		map = *((size_t *)s);
+		map = (map - LBYTE) & ~map & FBYTE;
+		if (map)
+			break ;
+		s += sizeof(size_t);
+	}
+	while (*s && !(map & 0xff))
+	{
+		s++;
+		map >>= 8;
+	}
+	return (s - start);
 }
